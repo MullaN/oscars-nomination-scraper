6c0d69326364e260b2e08756763bd3c36d00f147
MullaN <admullan@gmail.com>
2021-03-15 16:26:12 -0700

    completed list generation. need to fix newline characters in some fields

--- oscar-scraper.js
+++ oscar-scraper.js
@@ -18,8 +18,58 @@ const parseData = async () => {
     let awards = document.querySelectorAll('div.view-grouping-header > h2')
     awards = Array.from(awards).map(award => award.innerHTML)
     let awardDivs = document.querySelectorAll('div.view-grouping')
-    awardDivs = Array.from(awardDivs).filter(div => awards.some(award => div.children[0].children[0] && div.children[0].children[0].innerHTML === award))
-    
+
+    // narrow view-grouping divs to ones matching awards
+    awardDivs = Array.from(awardDivs).filter(div => awards.some(award => div.querySelector('h2') && div.querySelector('h2').innerHTML === award))
+    let movies = {}
+    let movie = ""
+    let people = ""
+    let nominees
+    let contents
+    let order = 0
+    let category = ""
+    awardDivs.forEach(div => {
+        category = div.querySelector('h2').textContent
+        // handle swapped order for actor/actress categories
+        if (category.startsWith('Actor') || category.startsWith('Actress')){
+            order = 1
+        } else {
+            order = 0
+        }
+
+        //grab nominee divs and remove empty content
+        contents = Array.from(div.getElementsByClassName('field-content')).filter(content => content.innerHTML !== "")
+        nominees = contents.map(nominee => nominee.textContent)
+
+        for(let i = 0; i < nominees.length; i += 2){
+            if (i % 2 === order){
+                movie = nominees[i]
+                people = nominees[i+1]
+            } else {
+                people = nominees[i]
+                movie = nominees[i+1]
+            }
+            if (movie in movies){
+                movies[movie].push({category, people})
+            } else {
+                movies[movie] = [{category, people}]
+            }
+        }
+    })
+    return movies
 }
 
-parseData()
+parseData().then(movies => {
+    const http = require('http');
+    const PORT = 3000;
+
+    const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(JSON.stringify(movies));
+    });
+
+    server.listen(PORT, () => {
+    console.log(`Server running at PORT:${PORT}/`);
+    });
+})
