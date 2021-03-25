f359e119a3cb7016a633cf59e3bd9cb63f546a50
MullaN <admullan@gmail.com>
2021-03-25 11:39:13 -0700

    trying to fix mongoose.connect

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DATABASE_URL, {useUnifiedTopology: true})
 const db = mongoose.connection
 
 db.on('error', (error) => console.error(error))
